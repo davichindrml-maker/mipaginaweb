0b17d27f520a5a54278aafb71abc11c00f691db8
davichindrml-maker <davichindrml@gmail.com>
2025-08-10 18:30:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 # Base de datos (usuarios y contraseñas)
 DIC= {
-    "usuario": ["claulopez", "Jacksoooon","Phdian", "Choco_Marii", "nadiashit","berenice", "pedrinho","davidlima","Esqueyosoyasi","Leohernandez","Lissete","ThePugG","Pollo","Yera"],
-    "contraseña": ["umpalumpa","moonwenee","Mimamimi","Ingatumais","vetealv","amoamifamilia","pecj6573","soydaviddd","DCOPN","Energia","cocacola","Pugcore","GaussJordan","Psique"]
+    "usuario": ["claulopez", "Jacksoooon","Phdian", "Choco_Marii", "nadiashit","berenice", "pedrinho","davidlima","Esqueyosoyasi","Leohernandez","Lissete","ThePugG","Pollo","Yera","Eugene"],
+    "contraseña": ["umpalumpa","moonwenee","Mimamimi","Ingatumais","vetealv","amoamifamilia","pecj6573","soydaviddd","DCOPN","Energia","cocacola","Pugcore","GaussJordan","Psique","ferrosolar"]
 }
 usuarios = pd.DataFrame(data=DIC)
 
@@ -127,6 +127,7 @@ def leer():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Toma el puerto que Render le indique
     app.run(host="0.0.0.0", port=port)
+
 
 
 
